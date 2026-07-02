962a302bf5b1ba7249c8a539b425029623b04b75
Pascal Monasse <pascal.monasse@enpc.fr>
2026-07-02 16:51:21 +0200

    IPOL demo: T-junctions

--- .ipol/run_ED.sh
+++ .ipol/run_ED.sh
@@ -6,7 +6,8 @@
 # -sigma: Gaussian blur
 # -valid: a conrario validation (true/false)
 # -leps: log10(epsilon) validation threshold (optional)
-# -sub: sublines (true/false, optional)
+# -sub: sublines (0,1,2)
+# -tjunc: compute T-junction (true/false)
 
 grad=$1
 gap=$2
@@ -14,10 +15,12 @@ length=$3
 sigma=$4
 valid="${5/false/}"
 if [ "$5" = "true" ]; then
-   valid="-e ${6:-0}" # default log10(eps)=0
-   if [ "$7" = "true" ]; then
-      valid="$valid -S";
-   fi
+    valid="-e ${6:-0}" # default log10(eps)=0
+    valid="$valid -S ${7:-0}";
 fi
 
-$bin/build/edgeDrawing $valid -g $grad -a $gap -l $length -s $sigma $input_0 edges.png
+if [ "$8" = "true" ]; then
+    tjunc="-t tjunc.png";
+fi
+
+$bin/build/edgeDrawing $valid -g $grad -a $gap -l $length -s $sigma $tjunc $input_0 edges.png
